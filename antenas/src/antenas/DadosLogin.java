3f3f3e58d9f0e8a8be0da87a0d6396f6283465ad
Vinícius Hoyer <vhoyer@live.com>
2019-11-05 09:21:09 -0300

    simplify compare method

--- antenas/src/antenas/DadosLogin.java
+++ antenas/src/antenas/DadosLogin.java
@@ -1,42 +1,31 @@
 package antenas;
 
 public class DadosLogin {
-	
 	private String email;
 	private String senha;
-	
-	
+
 	public DadosLogin(String email, String senha) {
 		this.email = email;
 		this.senha = senha;
 	}
 
-
 	public String getEmail() {
 		return email;
 	}
-
 
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
-
 	public String getSenha() {
 		return senha;
 	}
 
-
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
-	
-	public boolean compararDadosLogin(DadosLogin dl) {
-		if(email.equals(dl.email) && senha.equals(dl.senha)) {
-			return true;
-		} else {
-			return false;
-		}
-	}
 
+	public boolean compararDadosLogin(DadosLogin dl) {
+		return email.equals(dl.email) && senha.equals(dl.senha);
+	}
 }
